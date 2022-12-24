bbd2ec9f2548aaf2fedaa883d6aa3a097927178d
Mandar <mandarkahare@gmail.com>
2022-12-24 10:58:02 +0530

    Position of all

--- src/main/java/com/example/snakeladder/SnakeLadder.java
+++ src/main/java/com/example/snakeladder/SnakeLadder.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,8 @@ public class SnakeLadder extends Application {
     public static final int tileSize=40, height=10,width=10;
     int lowerLine=tileSize*height;
 
+    Player firstPlayer=new Player(tileSize, Color.BLACK,"Ankit");
+    Player secondPlayer=new Player(tileSize-10, Color.WHITE,"Sumit");
     Pane createContent(){
         Pane root=new Pane();
         root.setPrefSize(width*tileSize,height*tileSize+50);
@@ -42,8 +45,8 @@ public class SnakeLadder extends Application {
         playerTwoButton.setTranslateX(250);
         playerTwoButton.setTranslateY(lowerLine+20);
 
-        root.getChildren().addAll(boardImage,playerOneButton,playerTwoButton);
-
+        root.getChildren().addAll(boardImage,playerOneButton,playerTwoButton, firstPlayer.getCoin());
+        // secondPlayer.getCoin()
         return  root;
     }
 
