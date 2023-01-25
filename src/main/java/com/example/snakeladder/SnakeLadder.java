ca4dd1232104ef578a3e3e9453524336e2be5fa0
Mandar212 <mandarkahare@gmail.com>
2023-01-25 15:07:05 +0530

    start button

--- src/main/java/com/example/snakeladder/SnakeLadder.java
+++ src/main/java/com/example/snakeladder/SnakeLadder.java
@@ -1,9 +1,12 @@
 package com.example.snakeladder;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -15,12 +18,16 @@ import java.io.IOException;
 public class SnakeLadder extends Application {
     public static final int tileSize=40, height=10,width=10;
     int lowerLine=tileSize*height;
+    int diceValue;
+    Label rollDiceValueLabel;
+    Button startGameButton;
 
-    Player firstPlayer=new Player(tileSize, Color.BLACK,"Ankit");
-    Player secondPlayer=new Player(tileSize-10, Color.WHITE,"Sumit");
+    boolean firstPlayerTurn=true, secondPlayerTurn=false, gameStarted=false;
+    Player firstPlayer=new Player(tileSize, Color.BLACK,"Player 1");
+    Player secondPlayer=new Player(tileSize-10, Color.WHITE,"Player 2");
     Pane createContent(){
         Pane root=new Pane();
-        root.setPrefSize(width*tileSize,height*tileSize+50);
+        root.setPrefSize(width*tileSize,height*tileSize+100);
 
         for(int i=0;i<width;i++){
             for(int j=0;j<height;j++){
@@ -31,23 +38,90 @@ public class SnakeLadder extends Application {
             }
         }
 
-        Image img=new Image("D:\\Snake_Ladder\\src\\main\\snake.jpg");
+        Image img=new Image("D:\\projects\\Snake_Ladder\\src\\main\\snake.jpg");
         ImageView boardImage=new ImageView(img);
         boardImage.setImage(img);
         boardImage.setFitWidth(tileSize*width);
         boardImage.setFitHeight(tileSize*height);
 
         Button playerOneButton=new Button("Player One");
-        playerOneButton.setTranslateX(20);
-        playerOneButton.setTranslateY(lowerLine+20);
+        playerOneButton.setTranslateX(30);
+        playerOneButton.setTranslateY(lowerLine+15);
+        playerOneButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStarted){
+                    if(firstPlayerTurn){
+                        setDiceValue();
+                        firstPlayer.movePlayer(diceValue);
+                        if(firstPlayer.playerWon()!=null){
+                            rollDiceValueLabel.setText(firstPlayer.playerWon());
+                            firstPlayerTurn=true;
+                            secondPlayerTurn=false;
+                            gameStarted=false;
+                            startGameButton.setDisable(false);
+                            startGameButton.setText("Start Game");
+                        }
+                        firstPlayerTurn=false;
+                        secondPlayerTurn=true;
+                    }
+                }
+
+            }
+        });
 
         Button playerTwoButton=new Button("Player Two");
-        playerTwoButton.setTranslateX(250);
-        playerTwoButton.setTranslateY(lowerLine+20);
+        playerTwoButton.setTranslateX(290);
+        playerTwoButton.setTranslateY(lowerLine+15);
+        playerTwoButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStarted){
+                    if(secondPlayerTurn){
+                        setDiceValue();
+                        secondPlayer.movePlayer(diceValue);
+                        if(secondPlayer.playerWon()!=null){
+                            rollDiceValueLabel.setText(firstPlayer.playerWon());
+                            firstPlayerTurn=true;
+                            secondPlayerTurn=false;
+                            gameStarted=false;
+                            startGameButton.setText("StartGame");
+                        }
+                        firstPlayerTurn=true;
+                        secondPlayerTurn=false;
+                    }
+                }
 
-        root.getChildren().addAll(boardImage,playerOneButton,playerTwoButton, firstPlayer.getCoin());
-        // secondPlayer.getCoin()
+            }
+        });
+
+
+
+        startGameButton=new Button("Start");
+        startGameButton.setTranslateX(165);
+        startGameButton.setTranslateY(lowerLine+50);
+        startGameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                gameStarted=true;
+                startGameButton.setText("Ongoing Game");
+                startGameButton.setDisable(true);
+            }
+        });
+
+
+        rollDiceValueLabel=new Label("Start the Game!");
+        rollDiceValueLabel.setTranslateY(lowerLine+15);
+        rollDiceValueLabel.setTranslateX(150);
+
+        root.getChildren().addAll(boardImage,playerOneButton,playerTwoButton, firstPlayer.getCoin(),secondPlayer.getCoin(),rollDiceValueLabel,startGameButton);
+
         return  root;
+    }
+    private void setDiceValue(){
+        diceValue=(int)(Math.random()*6+1);
+        rollDiceValueLabel.setText("Dice Value :"+ diceValue);
     }
 
     @Override
